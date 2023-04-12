74f6797abe3a4d2f5e37016427bf2e186e33d507
Rosario <rosariogrieco89@gmail .com>
2023-04-12 11:38:45 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        RandomSort randomSort=new RandomSort();
+
+        int[] array= IntStream.generate(()-> new Random().nextInt(100)).limit(10).toArray();
+
+        for(int number : array){
+            System.out.print(number + " ");
+        }
+        System.out.println();
+
+        int counter= randomSort.sort(array);
+
+        for(int number : array){
+            System.out.print(number + " ");
+        }
+        System.out.println("Array shuffled for "+counter+" times");
+    }
+}
